7bf25d112207169f2686fe86f8adabf4ce36dec2
Liam <liam.elias.werner@campus.tu-berlin.de>
2026-04-25 12:14:11 +0200

    edited repetition settings menu

--- app/src/main/java/com/example/flashcards/RepetitionSettingsActivity.kt
+++ app/src/main/java/com/example/flashcards/RepetitionSettingsActivity.kt
@@ -24,7 +24,7 @@ class RepetitionSettingsActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_repetition_settings)
         }
 
     private fun saveSettings() {
